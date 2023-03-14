b3acf81c5a01c88f17ca1e1140eeedbfac59f0d8
Mohammad Ali Karimi <ali216k@gmail.com>
2023-03-14 20:59:06 +0000

    Added navbar and started on validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,9 +12,12 @@
 #  updated_at  :datetime         not null
 #  director_id :integer
 #
-class Movie < 
+class Movie < ApplicationRecord
   belongs_to :director
   has_many :characters
 
   has_many :cast, :through => :characters, :source => :actor
+
+  validates(:title, { :presence => true })
+
 end
